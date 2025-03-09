4d09751462ebc8835eecbdb0a874f01777a70817
mehakpreetlng99 <mehakpreet.kaur@lng-consultancy.com>
2025-03-09 13:03:30 +0530

    Add project files.

---
+++ VotingSystem.API/Models/Voter.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace VotingSystem.API.Models
+{
+    public class Voter
+    {
+        [Key]
+        public Guid VoterId { get; set; }
+
+        [Required, StringLength(100)]
+        public string VoterName { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public bool HasVoted { get; set; } = false;
+    }
+}
